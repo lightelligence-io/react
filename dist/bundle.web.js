012f56f95e7a93bf86a1ad348899ffb96af73a50
Heinz Hölzer <h.hoelzer@db-n.com>
2019-01-14 17:36:09 +0100

    update dist

--- dist/bundle.web.js
+++ dist/bundle.web.js
@@ -7,7 +7,7 @@
 		exports["lightelligence-react"] = factory();
 	else
 		root["lightelligence-react"] = factory();
-})(this, function() {
+})(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
