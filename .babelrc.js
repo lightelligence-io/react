a75ccfbd54d9929d4bc4c82e0711ac4bf997ee4d
Heinz Hölzer <h.hoelzer@db-n.com>
2019-01-15 22:05:57 +0100

    use babelrc.js file and try experimental commonjs output

---
+++ .babelrc.js
@@ -0,0 +1,18 @@
+const env = process.env.BABEL_ENV;
+console.log(env);
+
+const runtimeOptions =
+  env === 'esm' ? { useEsModules: true } : { useEsModules: false };
+
+const presets = [];
+const plugins = [
+  '@babel/plugin-proposal-class-properties',
+  '@babel/plugin-transform-react-jsx',
+  '@babel/plugin-proposal-object-rest-spread',
+  ['@babel/plugin-transform-runtime', runtimeOptions],
+];
+
+module.exports = {
+  plugins,
+  presets,
+};
