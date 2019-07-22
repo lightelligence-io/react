0ae050db655d06e0d28939469054c77abec0fceb
Jonathan Stoye <mail@jonathanstoye.de>
2019-07-22 11:18:42 +0200

    chore: improve testablity and customizability of modals

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
-import React, { PureComponent } from 'react';
-import { bool, node, func, oneOf, string } from 'prop-types';
+import React, { useEffect } from 'react';
+import { bool, node, func, oneOf, string, shape } from 'prop-types';
 import classnames from 'classnames';
 import { pascalize } from 'humps';
 import * as olt from '@lightelligence/styles';
@@ -12,80 +12,141 @@ import {
   MODAL_TYPE_NONE,
 } from './types';
 
-class Modal extends PureComponent {
-  static propTypes = {
-    children: node,
-    title: node,
-    type: oneOf([
-      MODAL_TYPE_ERROR,
-      MODAL_TYPE_SUCCESS,
-      MODAL_TYPE_INFO,
-      MODAL_TYPE_NONE,
-    ]),
-    open: bool.isRequired,
-    footer: node,
-    fullscreen: bool,
-    onClose: func,
-    closable: bool,
-    className: string,
-  };
-
-  static defaultProps = {
-    title: null,
-    children: null,
-    type: MODAL_TYPE_NONE,
-    fullscreen: false,
-    footer: null,
-    onClose: null,
-    closable: false,
-    className: null,
-  };
-
-  render() {
-    const {
-      children,
-      title,
-      type,
-      open,
-      closable,
-      fullscreen,
-      footer,
-      onClose,
-      className,
-      ...props
-    } = this.props;
-
+export const Modal = ({
+  children,
+  title,
+  type,
+  open,
+  closable,
+  fullscreen,
+  footer,
+  onClose,
+  className,
+  dialogProps,
+  modalProps,
+  contentProps,
+  footerProps,
+  headerProps,
+  closeButtonProps,
+  ...props
+}) => {
+  useEffect(() => {
     if (!isServerSideRendering) {
       document.documentElement.style.overflow = open ? 'hidden' : '';
     }
+  }, [open]);
 
-    return (
-      <section
-        className={classnames(
-          olt.Modal,
-          fullscreen && olt.ModalFullscreen,
-          type !== MODAL_TYPE_NONE && olt[`Modal${pascalize(type)}`],
-          open && olt.isOpen,
-          className,
-        )}
-        {...(closable
-          ? { onClick: (e) => e.target === e.currentTarget && onClose() }
-          : undefined)}
+  const onClickProps = closable
+    ? { onClick: (e) => e.target === e.currentTarget && onClose() }
+    : {};
+
+  const { className: modalClassName, ...otherModalProps } = modalProps;
+  const { className: dialogClassName, ...otherDialogProps } = dialogProps;
+  const { className: footerClassName, ...otherFooterProps } = footerProps;
+  const { className: headerClassName, ...otherHeaderProps } = headerProps;
+  const { className: contentClassName, ...otherContentProps } = contentProps;
+
+  const finalModalProps = {
+    ...otherModalProps,
+    ...onClickProps,
+  };
+  return (
+    <section
+      className={classnames(
+        olt.Modal,
+        fullscreen && olt.ModalFullscreen,
+        type !== MODAL_TYPE_NONE && olt[`Modal${pascalize(type)}`],
+        open && olt.isOpen,
+        className,
+        modalClassName,
+      )}
+      {...finalModalProps}
+    >
+      <div
+        className={classnames(olt.ModalDialog, dialogClassName)}
+        {...{ ...props, ...otherDialogProps }}
       >
-        <div className={olt.ModalDialog} {...props}>
-          {closable && !fullscreen && (
-            <Button icon="close" className={olt.ModalClose} onClick={onClose} />
-          )}
-          {title && <div className={olt.ModalHeader}>{title}</div>}
-          {children && <div className={olt.ModalContent}>{children}</div>}
-          {footer && <div className={olt.ModalFooter}>{footer}</div>}
-        </div>
-        {fullscreen && closable && (
-          <Button icon="close" className={olt.ModalClose} onClick={onClose} />
+        {closable && !fullscreen && (
+          <Button
+            icon="close"
+            className={olt.ModalClose}
+            onClick={onClose}
+            {...closeButtonProps}
+          />
         )}
-      </section>
-    );
-  }
-}
+        {title && (
+          <div
+            className={classnames(olt.ModalHeader, headerClassName)}
+            {...otherHeaderProps}
+          >
+            {title}
+          </div>
+        )}
+        {children && (
+          <div
+            className={classnames(olt.ModalContent, contentClassName)}
+            {...otherContentProps}
+          >
+            {children}
+          </div>
+        )}
+        {footer && (
+          <div
+            className={classnames(olt.ModalFooter, footerClassName)}
+            {...otherFooterProps}
+          >
+            {footer}
+          </div>
+        )}
+      </div>
+      {fullscreen && closable && (
+        <Button
+          icon="close"
+          className={olt.ModalClose}
+          onClick={onClose}
+          {...closeButtonProps}
+        />
+      )}
+    </section>
+  );
+};
 
-export { Modal };
+Modal.propTypes = {
+  children: node,
+  title: node,
+  type: oneOf([
+    MODAL_TYPE_ERROR,
+    MODAL_TYPE_SUCCESS,
+    MODAL_TYPE_INFO,
+    MODAL_TYPE_NONE,
+  ]),
+  open: bool.isRequired,
+  footer: node,
+  fullscreen: bool,
+  onClose: func,
+  closable: bool,
+  className: string,
+  dialogProps: shape({ className: string }),
+  modalProps: shape({ className: string }),
+  contentProps: shape({ className: string }),
+  footerProps: shape({ className: string }),
+  headerProps: shape({ className: string }),
+  closeButtonProps: shape({}),
+};
+
+Modal.defaultProps = {
+  title: null,
+  children: null,
+  type: MODAL_TYPE_NONE,
+  fullscreen: false,
+  footer: null,
+  onClose: null,
+  closable: false,
+  className: null,
+  dialogProps: {},
+  modalProps: {},
+  contentProps: {},
+  footerProps: {},
+  headerProps: {},
+  closeButtonProps: {},
+};
