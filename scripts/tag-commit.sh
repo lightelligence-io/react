33090940362ec06cf70ab3b6fb787750a7c5e9b5
Ivan Drinchev <drinchev@gmail.com>
2019-07-31 16:45:52 +0200

    release v0.7.2

--- scripts/tag-commit.sh
+++ scripts/tag-commit.sh
@@ -4,6 +4,5 @@ if [[ $TRAVIS_COMMIT_MESSAGE =~ ^release ]]; then
   git config --local user.name "lightelligence-bot"
   git config --local user.email "support@lightelligence.io"
   export TRAVIS_TAG=$(node -p "require('./package.json').version")
-  git tag $TRAVIS_TAG
 fi
 
