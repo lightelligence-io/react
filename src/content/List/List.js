921dd82727bf3f0f1e677c65ff5114f02a9e0396
Johannes Kettmann <jkettmann@gmx.net>
2019-08-02 14:37:16 +0200

    Added vertical prop to List

--- src/content/List/List.js
+++ src/content/List/List.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
-import { node, bool, string } from 'prop-types';
+import { node, bool, string, oneOf } from 'prop-types';
 import classnames from 'classnames';
+import { pascalize } from 'humps';
 import * as olt from '@lightelligence/styles';
 
 export class List extends PureComponent {
@@ -9,6 +10,7 @@ export class List extends PureComponent {
     className: string,
     ordered: bool,
     definition: bool,
+    vertical: oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
   };
 
   static defaultProps = {
@@ -16,21 +18,32 @@ export class List extends PureComponent {
     className: null,
     ordered: false,
     definition: false,
+    vertical: null,
   };
 
   render() {
-    const { children, className, ordered, definition, ...props } = this.props;
+    const {
+      children,
+      className,
+      ordered,
+      definition,
+      vertical,
+      ...props
+    } = this.props;
+
+    const classes = [
+      olt.List,
+      ordered && olt.ListOrdered,
+      definition && olt.ListDefinition,
+      className,
+    ];
+
+    if (vertical) {
+      classes.push(olt[`List${pascalize(vertical)}Vertical`]);
+    }
 
     return (
-      <ul
-        {...props}
-        className={classnames(
-          olt.List,
-          ordered && olt.ListOrdered,
-          definition && olt.ListDefinition,
-          className,
-        )}
-      >
+      <ul {...props} className={classnames(classes)}>
         {children}
       </ul>
     );
--- src/content/List/List.md
+++ src/content/List/List.md
@@ -36,3 +36,22 @@ import { List, ListItem, Tag } from '@lightelligence/react';
   <ListItem term="Status">online</ListItem>
 </List>
 ```
+
+Vertically stacked definition lists
+
+```jsx
+import { List, ListItem, Tag } from '@lightelligence/react';
+<List definition vertical="xl">
+  <ListItem term="Device Name">Waterproofed solar roof</ListItem>
+  <ListItem term="Device Type">Gateway</ListItem>
+  <ListItem term="Description">
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+  </ListItem>
+  <ListItem term="Firmware">DEV2S</ListItem>
+  <ListItem term="Tags">
+    <Tag>Light</Tag>
+    <Tag>Intelligence</Tag>
+  </ListItem>
+  <ListItem term="Status">online</ListItem>
+</List>
+```
