b1e95b44335cada0fe478d172ffdb3a7c14bbb1f
Johannes Kettmann <jkettmann@gmx.net>
2019-09-04 17:51:45 +0200

    Update react testing library and add user events package

--- src/tests/exports.test.js
+++ src/tests/exports.test.js
@@ -1,7 +1,6 @@
 import 'jest-dom/extend-expect';
-import 'react-testing-library/cleanup-after-each';
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
