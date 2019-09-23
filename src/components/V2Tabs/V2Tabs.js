731d86cee705134d18b77fbde93e0d5da30f2dde
Marco Blumendorf <marco@blumendorf.info>
2019-09-23 16:49:36 +0200

    add gradient logic

--- src/components/V2Tabs/V2Tabs.js
+++ src/components/V2Tabs/V2Tabs.js
@@ -1,72 +1,58 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 import { func, string, node } from 'prop-types';
 import classnames from 'classnames';
 import * as olt from '@lightelligence/styles';
 
-export class V2Tabs extends PureComponent {
-  static propTypes = {
-    value: string.isRequired,
-    onSelect: func.isRequired,
-    children: node.isRequired,
-    className: string,
-  };
+export const V2Tabs = (props) => {
+  const tabBar = useRef(null);
+  const [gradient, setGradient] = useState({ left: false, right: false });
 
-  static defaultProps = {
-    className: null,
-  };
+  const handleScroll = useCallback(() => {
+    const left = tabBar.current.scrollLeft > 0;
+    const right =
+      tabBar.current.scrollWidth -
+        tabBar.current.scrollLeft -
+        tabBar.current.clientWidth >
+      0;
+    setGradient({ left, right });
+  }, [tabBar]);
 
-  tabBar = null;
+  useEffect(() => {
+    handleScroll();
+    window.addEventListener('resize', handleScroll);
+  }, [tabBar, handleScroll]);
 
-  state = {
-    hasScrolledLeft: false,
-    canScrollRight: false,
-  };
+  const { value, onSelect, className, children, ...rest } = props;
 
-  componentDidMount() {
-    setTimeout(() => {
-      const overflows = () =>
-        this.tabBar && this.tabBar.scrollWidth > this.tabBar.clientWidth;
-      if (overflows()) this.setState({ canScrollRight: true });
-    }, 1000);
-  }
-
-  handleScroll = (e) => {
-    const tabs = e.target;
-    const hasScrolledLeft = tabs.scrollLeft > 0;
-    const canScrollRight =
-      tabs.scrollWidth - tabs.scrollLeft - tabs.clientWidth > 0;
-    this.setState({ hasScrolledLeft, canScrollRight });
-  };
-
-  render() {
-    const { value, onSelect, className, children, ...props } = this.props;
-
-    const { hasScrolledLeft, canScrollRight } = this.state;
-
-    return (
+  return (
+    <div {...rest} style={{ position: 'relative' }} onScroll={handleScroll}>
       <div
-        {...props}
-        style={{ position: 'relative' }}
-        onScroll={this.handleScroll}
-        ref={(e) => {
-          this.taBar = e;
-        }}
+        className={classnames(
+          !gradient.right && olt.V2Tabs,
+          gradient.right && olt.V2TabsForceScroll,
+          gradient.left && olt.hasGradientLeft,
+          gradient.right && olt.hasGradientRight,
+        )}
+        ref={tabBar}
       >
-        <div
-          className={classnames(
-            olt.V2Tabs,
-            hasScrolledLeft && olt.hasGradientLeft,
-            canScrollRight && olt.hasGradientRight,
-          )}
-        >
-          {React.Children.map(children, (element) =>
-            React.cloneElement(element, {
-              onSelect,
-              active: value === element.props.value,
-            }),
-          )}
-        </div>
+        {React.Children.map(children, (element) =>
+          React.cloneElement(element, {
+            onSelect,
+            active: value === element.props.value,
+          }),
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+V2Tabs.propTypes = {
+  value: string.isRequired,
+  onSelect: func.isRequired,
+  children: node.isRequired,
+  className: string,
+};
+
+V2Tabs.defaultProps = {
+  className: null,
+};
