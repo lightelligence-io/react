f64b5fa923f39703c21901ef009178471ef6a388
Marco Blumendorf <marco@blumendorf.info>
2019-10-01 17:18:25 +0200

    add new icons

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -4,20 +4,24 @@ import classnames from 'classnames';
 import { pascalize, camelize } from 'humps';
 import * as olt from '@lightelligence/styles';
 
-const Icon = ({ name, size, color, children, className, ...props }) => (
-  <i
-    {...props}
-    data-icon={name}
-    className={classnames(
-      olt.Icon,
-      color && olt[`Icon${pascalize(color)}`],
-      className,
-    )}
-    style={{
-      fontSize: size ? olt.theme.fontSize[camelize(size)] : undefined,
-    }}
-  />
-);
+const Icon = ({ name, size, color, children, className, ...props }) => {
+  return (
+    <i
+      {...props}
+      data-icon={name}
+      className={classnames(
+        olt.Icon,
+        color && olt[`Icon${pascalize(color)}`],
+        className,
+        name && olt[`Icon${pascalize(name)}`],
+        `${pascalize(name)}`,
+      )}
+      style={{
+        fontSize: size ? olt.theme.fontSize[camelize(size)] : undefined,
+      }}
+    />
+  );
+};
 
 Icon.propTypes = {
   name: string.isRequired,
