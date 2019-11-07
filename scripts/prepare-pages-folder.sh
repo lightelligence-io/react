8f1dd621d0db6f217b4176988dd43fa36ff42fd3
Marco Blumendorf <marco@blumendorf.info>
2019-11-07 17:38:19 +0100

    add deployment scripts

--- scripts/prepare-pages-folder.sh
+++ scripts/prepare-pages-folder.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 
+echo "starting gh-pages creation ..."
 VERSION=$(node -p "require('./package.json').version")
 
-if [[ $TRAVIS_COMMIT_MESSAGE =~ ^release ]]; then
-  echo -n 'release commit: creating release docs ... '
-  mkdir -p docs/releases/$VERSION
-  ls docs/ | grep -v releases | xargs -I{} cp -r docs/{} docs/releases/$VERSION
-  echo 'done.'
-fi
-
-
-if [[ $TRAVIS_BRANCH == alpha ]]; then
-  echo -n 'branch alpha: creating alpha docs ... '
-  mkdir docs/$VERSION
-  ls docs/ | grep -v -e releases -e $VERSION | xargs -I{} mv docs/{} docs/$VERSION
-  echo 'done.'
-fi
-
-# checkout current gh-poages branch and add new docs
-echo 'creating gh-pages folder ...'
-mkdir gh-pages
-echo 'checking out gh-pages branch ...'
+# checkout current gh-pages branch
+echo -n "checking out - https://github.com/$TRAVIS_REPO_SLUG gh-pages branch ... "
 git clone -b gh-pages --single-branch https://github.com/$TRAVIS_REPO_SLUG gh-pages
-echo 'adding new documents ...'
-cp -r docs/* gh-pages
-echo 'done.'
+echo "done"
+
+echo -n "ensuring folders exist... "
+mkdir -p gh-pages/releases gh-pages/alpha gh-pages/master
+echo "done"
+
+# create release documentation and move to gh-pages/releases/x.x.x
+if [[ $TRAVIS_COMMIT_MESSAGE =~ ^release ]]; then
+  echo -n "release commit - creating release docs for version $VERSION ... "
+  rsync -r --delete ./docs/ gh-pages/releases/$VERSION
+  node "./scripts/addNewVersion.js"
+  if [[ $TRAVIS_BRANCH == master ]]; then
+    node "./scripts/rewriteIndex.js"
+  fi
+  echo "done."
+fi
+
+# create alpha documentation and move it to gh-pages/alpha
+if [[ $TRAVIS_BRANCH == alpha ]]; then
+  echo -n "branch alpha - creating alpha docs ... "
+  rsync -r --delete ./docs/ gh-pages/alpha/
+  echo "done."
+fi
+
+# create master documentation and move it to gh-pages/master
+if [[ $TRAVIS_BRANCH == master ]]; then
+  echo -n "branch master - creating master docs ... "
+  rsync -r --delete ./docs/ gh-pages/master/
+  echo "done."
+fi
+
+echo "gh-pages creation ... done."
