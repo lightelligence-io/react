f420d7cac8a3f562de189e93edac0f449825b04b
Manuel Bieh <github@manuelbieh.de>
2019-11-25 15:26:48 +0000

    chore: rewrite classes to hooks

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { oneOf, string } from 'prop-types';
 import classnames from 'classnames';
 import * as olt from '@lightelligence/styles';
@@ -10,21 +10,8 @@ import {
   AVATAR_SIZE_MEDIUM,
 } from '../../constants';
 
-export class Avatar extends PureComponent {
-  static propTypes = {
-    name: string.isRequired,
-    className: string,
-    size: oneOf([AVATAR_SIZE_MEDIUM, AVATAR_SIZE_LARGE]),
-    type: oneOf([AVATAR_TYPE_USER, AVATAR_TYPE_TENANT]).isRequired,
-  };
-
-  static defaultProps = {
-    className: null,
-    size: AVATAR_SIZE_MEDIUM,
-  };
-
-  render() {
-    const { type, size, name, className, ...props } = this.props;
+export const Avatar = React.memo(
+  ({ type, size = AVATAR_SIZE_MEDIUM, name, className = null, ...props }) => {
     const shortName = name.substr(0, 1);
 
     return (
@@ -40,5 +27,17 @@ export class Avatar extends PureComponent {
         {shortName}
       </i>
     );
-  }
-}
+  },
+);
+
+Avatar.propTypes = {
+  name: string.isRequired,
+  className: string,
+  size: oneOf([AVATAR_SIZE_MEDIUM, AVATAR_SIZE_LARGE]),
+  type: oneOf([AVATAR_TYPE_USER, AVATAR_TYPE_TENANT]).isRequired,
+};
+
+Avatar.defaultProps = {
+  className: null,
+  size: AVATAR_SIZE_MEDIUM,
+};
