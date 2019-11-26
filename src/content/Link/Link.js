9cdddc337e5bf58f315ae92129abae31de4f2f3d
Marco Blumendorf <marco@blumendorf.info>
2019-11-26 14:58:54 +0100

    Feature: add color to links

--- src/content/Link/Link.js
+++ src/content/Link/Link.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { string, bool, node } from 'prop-types';
+import { pascalize } from 'humps';
 import { NavLink, matchPath } from 'react-router-dom';
 import classnames from 'classnames';
 import * as olt from '@lightelligence/styles';
@@ -7,35 +8,43 @@ import * as olt from '@lightelligence/styles';
 /**
  * Combines router navigation with standard link, seasoned with some spicy color schemes
  */
-const Link = React.memo(({ to, children, className, normal, ...props }) => {
-  const match = matchPath(to, {
-    path: '/',
-    exact: false,
-  });
-  const Element = match ? NavLink : 'a';
+const Link = React.memo(
+  ({ to, children, className, normal, color, ...props }) => {
+    const match = matchPath(to, {
+      path: '/',
+      exact: false,
+    });
+    const Element = match ? NavLink : 'a';
 
-  return (
-    <Element
-      {...{
-        ...(match ? { to } : { href: to }),
-        ...props,
-        className: classnames(!normal && olt.Link, className),
-      }}
-    >
-      {children}
-    </Element>
-  );
-});
+    return (
+      <Element
+        {...{
+          ...(match ? { to } : { href: to }),
+          ...props,
+          className: classnames(
+            !normal && olt.Link,
+            color && olt[`uColor${pascalize(color)}`],
+            className,
+          ),
+        }}
+      >
+        {children}
+      </Element>
+    );
+  },
+);
 
 Link.propTypes = {
   to: string.isRequired,
   normal: bool,
+  color: string,
   children: node,
   className: string,
 };
 
 Link.defaultProps = {
   normal: false,
+  color: null,
   className: null,
   children: null,
 };
